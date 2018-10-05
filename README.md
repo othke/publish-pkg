04febce3af240ae87ddbbdee378f8b55d34859fa
Olivier Thomas <olivier.thomas.carto@gmail.com>
2018-10-05 15:57:33 +0200

    Remove default bucket name into package.json and add list method

--- README.md
+++ README.md
@@ -1,48 +1,36 @@
 # Publish a npm package to a S3 Bucket
 
 This package is a helper to publish npm package to a S3 Bucket.
+This is **less convenient than use npm repository** but if we want to keep some packages out of the public this is a quick and functionnal **workaround** usinng only S3 and no database.
 
-## Setup
+## Install
 
 ```bash
-npm install -g publish-pkg
+# You can install this package globally to avoid installing it for all your privates packages. 
+npm install -g publish-pkg-s3
 ```
 
 ## How it works
 
-You should set the s3 bucket name into the **package.json** of your package like that
+You should execute the script in a root package directory. The script will list all the directories and files inside the package directory excepted the node_modules directory.
 
-```json
-"bucket": {
-    "name": "repo-npm.dev"
-  }
-```
-
-Then you can execute this command to publish your package. This command should be execute at the root of the package
+Then it will targz the content of the package and upload it on your S3 bucket. The name of the package and the version are picked into your **package.json**. If a the same version already exists the script will not upload your package except you ask it to force the upload.
 
 ```bash
-publish-pkg
+# upload a package
+publish-package-s3 publish <bucketName>
+
+# output
+# Package: packageName version: 1.0.0 will be published
+# Package: packageName version: 1.0.0 published
+# URL: https://s3.<region>.amazonaws.com/<bucket>/<package>-<version>.tgz
+
+# upload force a package
+publish-package-s3 publish <bucketName> --force
+
+# list already uploaded packages with the same name
+publish-package-s3 name <bucketName>
 ```
 
-Your package will have the following name:
-
-```bash
-https://s3-eu-west-1.amazonaws.com/{your-bucket-name}/{package-name}-{version}.tgz
-```
-
-Your users should have a public read access on the bucket. So You need to configure your s3 bucket consequently.
-
-```json
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "AddPerm",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": ["s3:GetObject"],
-      "Resource": ["arn:aws:s3:::your-bucket/*"]
-    }
-  ]
-}
-```
+You can use npm link to check that your package work well before upload your package on S3.
+See [npm link](https://docs.npmjs.com/cli/link).
--- src/cli.js
+++ src/cli.js
@@ -7,20 +7,25 @@ const fs = require('fs-extra');
 const path = require('path');
 const { PassThrough } = require('stream');
 
+const program = require('commander');
 const chalk = require('chalk');
 const rimraf = require('rimraf');
 const tar = require('tar');
 const AWS = require('aws-sdk');
 
-const PACKAGE_DIR = process.cwd();
-const PACKAGE_BUILD = path.join(PACKAGE_DIR, '.build');
-const PACKAGE_BUILD_TAR = path.join(PACKAGE_BUILD, 'package');
-const pkg = require(path.join(PACKAGE_DIR, 'package.json'));
+const BUILD_NAME = '.build';
+const PACKAGE_NAME = 'package';
 
+const PACKAGE_ROOT_DIR = process.cwd();
+const PACKAGE_BUILD_DIR = path.join(PACKAGE_ROOT_DIR, BUILD_NAME);
+const PACKAGE_BUILD_TAR_DIR = path.join(PACKAGE_BUILD_DIR, PACKAGE_NAME);
+const PKG = require(path.join(PACKAGE_ROOT_DIR, 'package.json'));
+
+const PKG_FORMAT_REGEX = /^([\w-]*)-(\d.\d.\d)(.tgz)$/;
 const s3 = new AWS.S3();
 
 /**
- * Clean a directory
+ * Remove a directory
  * @param {*} path
  */
 const remove = path =>
@@ -35,11 +40,11 @@ const remove = path =>
   });
 
 /**
- * Give the list of file to includes into the package
+ * List the files to include into the package
  * @param {*} blackList
  */
 const listPackageFiles = (blackList = ['node_modules']) => {
-  const files = fs.readdirSync(PACKAGE_DIR);
+  const files = fs.readdirSync(PACKAGE_ROOT_DIR);
   return files.filter(file => !blackList.includes(file));
 };
 
@@ -47,63 +52,145 @@ const listPackageFiles = (blackList = ['node_modules']) => {
  * Build the package folder and copy the source files
  * @param {*} files
  */
-const makePackageFolder = async files => {
-  fs.mkdirSync(PACKAGE_BUILD);
-  fs.mkdirSync(PACKAGE_BUILD_TAR);
+const buildPackageDirectory = async files => {
+  fs.mkdirSync(PACKAGE_BUILD_DIR);
+  fs.mkdirSync(PACKAGE_BUILD_TAR_DIR);
   await Promise.all(
     files.map(file => {
-      const from = path.join(PACKAGE_DIR, file);
-      const to = path.join(PACKAGE_BUILD_TAR, file);
+      const from = path.join(PACKAGE_ROOT_DIR, file);
+      const to = path.join(PACKAGE_BUILD_TAR_DIR, file);
       return fs.copy(from, to);
     })
   );
 };
 
-const checkPackageExists = async () => {
-  await s3.headObject().promise();
+const checkPackageAlreadyExists = async (bucketName, key) => {
+  try {
+    const params = {
+      Bucket: bucketName,
+      Key: key
+    };
+    await s3.headObject(params).promise();
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+/**
+ * List all packages version with the given name
+ * @param {*} bucketName
+ * @param {*} keyPrefix
+ */
+const listObjects = async (bucketName, keyPrefix) => {
+  let pkg = [];
+  const fetchObjects = async marker => {
+    const params = {
+      Bucket: bucketName,
+      Marker: marker || undefined
+    };
+    const {
+      IsTruncated: hasNext,
+      NextMarker: nextMarker,
+      Contents: contents
+    } = await s3.listObjects(params).promise();
+    const samePackage = contents.filter(({ Key }) => {
+      const [full, name, version] = PKG_FORMAT_REGEX.exec(Key);
+      return name === keyPrefix;
+    });
+    pkg = pkg.concat(samePackage);
+    if (hasNext) {
+      pkg = pkg.concat(await fetch(nextMarker));
+    }
+    return pkg;
+  };
+  return await fetchObjects();
 };
 
 /**
  * Upload the package on S3
  * @param {*} tarFiles
  */
-const uploadPackage = async (tarFiles, key) => {
-  process.chdir(PACKAGE_BUILD);
+const uploadPackage = async (tarFiles, bucketName, key) => {
+  process.chdir(PACKAGE_BUILD_DIR);
   const pass = new PassThrough();
   tar.c({ gzip: true }, tarFiles).pipe(pass);
   const params = {
-    Bucket: pkg.bucket.name,
+    Bucket: bucketName,
     Key: key,
     Body: pass
   };
-  await s3.upload(params).promise();
-  process.chdir(PACKAGE_DIR);
+  const pkg = await s3.upload(params).promise();
+  process.chdir(PACKAGE_ROOT_DIR);
+  return pkg;
 };
 
 /**
  * Publish the package on S3
  */
-const publish = async () => {
-  const { name, version } = pkg;
+const publish = async (bucketName, force) => {
+  const { name, version } = PKG;
   if (!name || !version) {
     console.log(chalk.red('package must have name and version'));
     process.exit(1);
   }
   const key = `${name}-${version}.tgz`;
+  const exists = await checkPackageAlreadyExists(bucketName, key);
+  if (exists && !force) {
+    console.log(chalk.red(`Package: ${name} version: ${version} already exists`));
+    console.log(chalk.red(`use --force argument to override the package`));
+    process.exit(1);
+  }
   console.log(chalk.green(`Package: ${name} version: ${version} will be published`));
 
   // clean previous build and copy all package files into the .build/package dir
-  await remove(PACKAGE_BUILD);
-  const packageFiles = listPackageFiles();
-  await makePackageFolder(packageFiles);
+  await remove(PACKAGE_BUILD_DIR);
+  const packageFiles = listPackageFiles(name);
+  await buildPackageDirectory(packageFiles);
   // upload package
-  await uploadPackage([PACKAGE_BUILD], key);
+  const pkg = await uploadPackage([PACKAGE_NAME], bucketName, key);
 
   // clean the build
-  await remove(PACKAGE_BUILD);
+  await remove(PACKAGE_BUILD_DIR);
   console.log(chalk.green(`Package: ${name} version: ${version} published`));
+  console.log(chalk.green(`URL: ${pkg.Location}`));
 };
 
-publish().catch(err => {
-  console.error(chalk.red(err));
-});
+/**
+ * List all versions of the current package
+ */
+const list = async bucketName => {
+  const { name, version } = PKG;
+  if (!name || !version) {
+    console.log(chalk.red('package must have name and version'));
+    process.exit(1);
+  }
+  const existingPackage = await listObjects(bucketName, name);
+  existingPackage.map(pkg => {
+    console.log(chalk.green(`Name: ${pkg.Key}`));
+    console.log(chalk.green(`Last Modification: ${pkg.LastModified}`));
+    console.log('');
+  });
+};
+
+program.version(PKG.version).description(PKG.description);
+program
+  .command('publish <bucketName>')
+  .option('-f, --force', 'Force override package')
+  .description('Publish a package')
+  .action((bucketName, cmd) => {
+    publish(bucketName, cmd.force).catch(err => {
+      console.error(chalk.red(err));
+    });
+  });
+
+program
+  .command('list <bucketName>')
+  .description('List all versions of the current package')
+  .action((bucketName, cmd) => {
+    list(bucketName).catch(err => {
+      console.error(chalk.red(err));
+    });
+  });
+
+program.parse(process.argv);
